d08ad06983604ae5410a4f876d14f9ae55ac0056
Tianhao Zhang <zhangth1990@hotmail.com>
2024-05-19 22:18:40 -0600

    step 2 css

--- src/components/createProperty/StepTwo.tsx
+++ src/components/createProperty/StepTwo.tsx
@@ -32,135 +32,137 @@ import { LiaWarehouseSolid } from "react-icons/lia";
 function StepTwo() {
     return (
         <Container>
-            <div className="text-4xl font-bold mt-20 mb-7">
-                Which of these best describes your place?
-            </div>
-            <FlexContainer>
-                <Border>
-                    <MdOutlineHouse size={45} />
-                    House
-                </Border>
-                <Border>
-                    <PiBuildingApartment size={45} />
-                    Apartment
-                </Border>
-                <Border>
-                    <PiBarn size={45} />
-                    Barn
-                </Border>
-                <Border>
-                    <TbCoffee size={45} />
-                    Bed & Breakfast
-                </Border>
-                <Border>
-                    <LuSailboat size={45} />
-                    Boat
-                </Border>
-                <Border>
-                    <MdCabin size={45} />
-                    Cabin
-                </Border>
-                <Border>
-                    <TbCamper size={45} />
-                    Camper/RV
-                </Border>
-                <Border>
-                    <PiBuildings size={45} />
-                    Casa particular
-                </Border>
-                <Border>
-                    <GiCastle size={45} />
-                    Castle
-                </Border>
-                <Border>
-                    <GiCaveEntrance size={45} />
-                    Cave
-                </Border>
-                <Border>
-                    <PiShippingContainerDuotone size={45} />
-                    Container
-                </Border>
-                <Border>
-                    <AiFillAlert size={45} />
-                    Cycladic home
-                </Border>
-                <Border>
-                    <GiGreenhouse size={45} />
-                    Dammuso
-                </Border>
-                <Border>
-                    <FaLandmarkDome size={45} />
-                    Dome
-                </Border>
-                <Border>
-                    <RiEarthquakeLine size={45} />
-                    Earth home
-                </Border>
-                <Border>
-                    <PiFarm size={45} />
-                    Farm
-                </Border>
-                <Border>
-                    <GiFamilyHouse size={45} />
-                    Guesthouse
-                </Border>
-                <Border>
-                    <FaHotel size={45} />
-                    Hotel
-                </Border>
-                <Border>
-                    <MdOutlineHouseboat size={45} />
-                    Houseboat
-                </Border>
-                <Border>
-                    <FaHouseSignal size={45} />
-                    Kezhan
-                </Border>
-                <Border>
-                    <BiBuildingHouse size={45} />
-                    Minsu
-                </Border>
-                <Border>
-                    <PiLighthouseLight size={45} />
-                    Riad
-                </Border>
-                <Border>
-                    <LiaWarehouseSolid size={45} />
-                    Ryokan
-                </Border>
-                <Border>
-                    <FaCaravan size={45} />
-                    Shepherd's hut
-                </Border>
-                <Border>
-                    <GiCampingTent size={45} />
-                    Tent
-                </Border>
-                <Border>
-                    <MdOutlineOtherHouses size={45} />
-                    Tiny home
-                </Border>
-                <Border>
-                    <GiHeartTower size={45} />
-                    Tower
-                </Border>
-                <Border>
-                    <GiTreehouse size={45} />
-                    Tree house
-                </Border>
-                <Border>
-                    <GiMushroomHouse size={45} />
-                    Trullo
-                </Border>
-                <Border>
-                    <PiWindmill size={45} />
-                    Windmill
-                </Border>
-                <Border>
-                    <FaHouseUser size={45} />
-                    Yurt
-                </Border>
-            </FlexContainer>
+            <TitleAndFlex>
+                <div className="text-4xl font-bold mt-6 mb-7">
+                    Which of these best describes your place?
+                </div>
+                <FlexContainer>
+                    <Border>
+                        <MdOutlineHouse size={45} />
+                        House
+                    </Border>
+                    <Border>
+                        <PiBuildingApartment size={45} />
+                        Apartment
+                    </Border>
+                    <Border>
+                        <PiBarn size={45} />
+                        Barn
+                    </Border>
+                    <Border>
+                        <TbCoffee size={45} />
+                        Bed & Breakfast
+                    </Border>
+                    <Border>
+                        <LuSailboat size={45} />
+                        Boat
+                    </Border>
+                    <Border>
+                        <MdCabin size={45} />
+                        Cabin
+                    </Border>
+                    <Border>
+                        <TbCamper size={45} />
+                        Camper/RV
+                    </Border>
+                    <Border>
+                        <PiBuildings size={45} />
+                        Casa particular
+                    </Border>
+                    <Border>
+                        <GiCastle size={45} />
+                        Castle
+                    </Border>
+                    <Border>
+                        <GiCaveEntrance size={45} />
+                        Cave
+                    </Border>
+                    <Border>
+                        <PiShippingContainerDuotone size={45} />
+                        Container
+                    </Border>
+                    <Border>
+                        <AiFillAlert size={45} />
+                        Cycladic home
+                    </Border>
+                    <Border>
+                        <GiGreenhouse size={45} />
+                        Dammuso
+                    </Border>
+                    <Border>
+                        <FaLandmarkDome size={45} />
+                        Dome
+                    </Border>
+                    <Border>
+                        <RiEarthquakeLine size={45} />
+                        Earth home
+                    </Border>
+                    <Border>
+                        <PiFarm size={45} />
+                        Farm
+                    </Border>
+                    <Border>
+                        <GiFamilyHouse size={45} />
+                        Guesthouse
+                    </Border>
+                    <Border>
+                        <FaHotel size={45} />
+                        Hotel
+                    </Border>
+                    <Border>
+                        <MdOutlineHouseboat size={45} />
+                        Houseboat
+                    </Border>
+                    <Border>
+                        <FaHouseSignal size={45} />
+                        Kezhan
+                    </Border>
+                    <Border>
+                        <BiBuildingHouse size={45} />
+                        Minsu
+                    </Border>
+                    <Border>
+                        <PiLighthouseLight size={45} />
+                        Riad
+                    </Border>
+                    <Border>
+                        <LiaWarehouseSolid size={45} />
+                        Ryokan
+                    </Border>
+                    <Border>
+                        <FaCaravan size={45} />
+                        Shepherd's hut
+                    </Border>
+                    <Border>
+                        <GiCampingTent size={45} />
+                        Tent
+                    </Border>
+                    <Border>
+                        <MdOutlineOtherHouses size={45} />
+                        Tiny home
+                    </Border>
+                    <Border>
+                        <GiHeartTower size={45} />
+                        Tower
+                    </Border>
+                    <Border>
+                        <GiTreehouse size={45} />
+                        Tree house
+                    </Border>
+                    <Border>
+                        <GiMushroomHouse size={45} />
+                        Trullo
+                    </Border>
+                    <Border>
+                        <PiWindmill size={45} />
+                        Windmill
+                    </Border>
+                    <Border>
+                        <FaHouseUser size={45} />
+                        Yurt
+                    </Border>
+                </FlexContainer>
+            </TitleAndFlex>
         </Container>
     );
 }
@@ -175,26 +177,32 @@ const Container = styled.div`
     align-items: center;
     flex-direction: column;
 `;
-const FlexContainer = styled.div`
-    display: flex;
-    flex-wrap: wrap;
+const TitleAndFlex = styled.div`
     width: 60%;
     height: 72%;
-    gap: 15px;
     overflow-x: hidden;
     overflow-y: scroll;
     &::-webkit-scrollbar {
         display: none;
     }
 `;
+const FlexContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 15px;
+`;
 const Border = styled.div`
     border-radius: 10px;
-    border-width: 3px;
+    border-width: 1px;
     height: 120px;
     width: 200px;
-    border-color: black;
+    border-color: gray;
     font-size: larger;
     font-weight: 500;
     padding: 10px;
     width: 30%;
+    &:hover {
+        border-color: black;
+        border-width: 3px;
+    }
 `;
